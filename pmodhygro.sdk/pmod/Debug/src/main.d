504a9eccccbce8fe729a9c686e0901dddf53133d
Satinderpalsingh Sodhi <f20190656@hyderabad.bits-pilani.ac.in>
2022-04-11 02:23:57 +0530

    initial commit

---
+++ pmodhygro.sdk/pmod/Debug/src/main.d
@@ -0,0 +1,52 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../pmod_bsp/ps7_cortexa9_0/include/PmodGYRO.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xspi_l.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xspi.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../pmod_bsp/ps7_cortexa9_0/include/xil_printf.h
+
+../../pmod_bsp/ps7_cortexa9_0/include/PmodGYRO.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xspi_l.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xspi.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../pmod_bsp/ps7_cortexa9_0/include/xil_printf.h:
